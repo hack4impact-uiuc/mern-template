67c3ad2d83a1c2b70f085a19ca7d59dd5b1734b3
Arpan Laha <arpanlaha99@gmail.com>
2021-01-18 04:18:16 +0000

    run format

--- api/src/middleware/errorWrap.js
+++ api/src/middleware/errorWrap.js
@@ -2,6 +2,7 @@
  * Middleware to allow us to safely handle errors in async/await code
  * without wrapping each route in try...catch blocks
  */
-const errorWrap = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+const errorWrap = fn => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
 module.exports = errorWrap;
